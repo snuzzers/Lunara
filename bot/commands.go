436c3846dd67db763b99f4525561bde14d1b23b0
snuzzers <defaultsnuzzers@gmail.com>
2023-06-03 14:52:33 -0600

    Subcommand example

--- bot/commands.go
+++ bot/commands.go
@@ -80,3 +80,31 @@ func CommandWithOptions(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		},
 	})
 }
+
+func CommandWithSubcommands(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	options := i.ApplicationCommandData().Options
+	content := ""
+
+	// As you can see, names of subcommands (nested, top-level)
+	// and subcommand groups are provided through the arguments.
+	switch options[0].Name {
+	case "subcommand":
+		content = "The top-level subcommand is executed. Now try to execute the nested one."
+	case "subcommand-group":
+		options = options[0].Options
+		switch options[0].Name {
+		case "nested-subcommand":
+			content = "Nice, now you know how to execute nested commands too"
+		default:
+			content = "Oops, something went wrong.\n" +
+				"Hol' up, you aren't supposed to see this message."
+		}
+	}
+
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: content,
+		},
+	})
+}
